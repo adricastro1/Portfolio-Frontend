8305e47fe28f7b751e7b9ef6741f903f5b92fd7b
adricastro1 <adricastro1196@gmail.com>
2023-05-19 21:12:08 -0400

    add resume icon

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,25 +1,30 @@
 import "./Home.css"
 import { FaGithub, FaLinkedin } from "react-icons/fa"
+import { HiDocumentDownload } from "react-icons/hi";
 
 function Home(props) {
   return (
     <section className="Home" id="home">
-      <img src="../../imgs/home-img.jpeg"/>
+      <img src="../../imgs/home-img.jpeg" />
       <div className="home-info">
         <h1>Adriana Castro</h1>
         <h3>Software Developer</h3>
         <div className="icons">
           <div className="icons-1">
             <a href="https://github.com/adricastro1" target="_blank" rel="noreferrer noopener">
-              <FaGithub /> 
+              <FaGithub />
             </a>
-              <p>Github</p>
+            <p>Github</p>
           </div>
           <div className="icons-1">
             <a href="https://www.linkedin.com/in/adriana-s-castro/" target="_blank" rel="noreferrer noopener">
-              <FaLinkedin /> 
+              <FaLinkedin />
             </a>
-              <p>LinkedIn</p>
+            <p>LinkedIn</p>
+          </div>
+          <div className="icons-1">
+            <a><HiDocumentDownload /></a>
+            <p>Resume</p>
           </div>
         </div>
       </div>
