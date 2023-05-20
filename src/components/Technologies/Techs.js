c44ff50ab22f33ae534afc976afbd9354dcb386c
adricastro1 <adricastro1196@gmail.com>
2023-05-19 20:56:57 -0400

    technologies page

--- src/components/Technologies/Techs.js
+++ src/components/Technologies/Techs.js
@@ -1,11 +1,101 @@
 import "./Techs.css"
 
 function Technologies(props) {
-  return (
-    <section className="Tech" id="tech">
-    <h1>hello</h1>
-    </section>
-  );
+    return (
+        <section className="Tech" id="tech">
+            <div>
+                <i class="devicon-css3-plain"></i>
+
+                <h2>CSS</h2>
+            </div>
+
+            <div>
+                <i class="devicon-html5-plain"></i>
+                <h2>HTML</h2>
+            </div>
+
+            <div>
+                <i class="devicon-javascript-plain"></i>
+                <h2>JavaScript</h2>
+            </div>
+
+            <div>
+                <i class="devicon-nodejs-plain"></i>
+                <h2>NodeJS</h2>
+            </div>
+
+            <div>
+                <i class="devicon-python-plain"></i>
+                <h2>Python</h2>
+            </div>
+
+            <div>
+                <i class="devicon-django-plain"></i>
+                <h2>Django</h2>
+            </div>
+
+            <div>
+                <i class="devicon-bootstrap-plain"></i>
+                <h2>Bootstrap</h2>
+            </div>
+
+            <div>
+                <i class="devicon-express-original"></i>
+                <h2>Express</h2>
+            </div>
+
+            <div>
+                <i class="devicon-flask-original"></i>
+                <h2>Flask</h2>
+            </div>
+
+            <div>
+                <i class="devicon-github-original"></i>
+                <h2>Github</h2>
+            </div>
+
+            <div>
+                <i class="devicon-heroku-original"></i>
+                <h2>Heroku</h2>
+            </div>
+
+            <div>
+                <i class="devicon-mongodb-plain"></i>
+                <h2>MongoDb</h2>
+            </div>
+
+            <div>
+                <i class="devicon-mysql-plain"></i>
+                <h2>MySQL</h2>
+            </div>
+
+            <div>
+                <i class="devicon-postgresql-plain"></i>
+                <h2>PostgresSQL</h2>
+            </div>
+
+            <div>
+                <i class="devicon-react-original"></i>
+                <h2>React</h2>
+            </div>
+
+            <div>
+                <i class="devicon-tailwindcss-original-wordmark"></i>
+                <h2>Tailwind</h2>
+            </div>
+
+            <div>
+
+            <i class="devicon-npm-original-wordmark"></i>
+                          <h2>NPM</h2>
+            </div>
+            
+            <div>
+                <i class="devicon-vscode-plain"></i>
+                <h2>VSCode</h2>
+            </div>
+        </section>
+    );
 }
 
 export default Technologies;
